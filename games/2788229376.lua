c7908427fd169664143cd70ea5b9d4029b0bc797
fraudz <105895090+fraudz@users.noreply.github.com>
2022-05-19 17:48:09 -0700

    Update 2788229376.lua

--- games/2788229376.lua
+++ games/2788229376.lua
@@ -1,2 +1,14 @@
---//Da hood
-loadstring(game:HttpGet('https://raw.githubusercontent.com/laagginq/fraudz/main/loadstring.lua'))()
+--//Load fraud
+local supportedgames = {
+    2788229376, --da hood
+}
+if table.find(supportedgames,game.PlaceId) then 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/games/'..game.PlaceId..'.lua'))()
+else
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "fraud",
+        Text = "Game Unsupported\nWe're sorry :(",
+        Duration = 10,
+        Button1 = "Okay",
+    })
+end
