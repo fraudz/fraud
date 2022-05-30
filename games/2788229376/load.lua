ed8b6dc9eb274741179071966fe3718ab59eaa7c
fraudz <105895090+fraudz@users.noreply.github.com>
2022-05-29 18:53:24 -0700

    Update load.lua

--- games/2788229376/load.lua
+++ games/2788229376/load.lua
@@ -14,7 +14,7 @@ if sets == nil then
 end
 
 if sets.performace == false then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/normal"..sets.version..".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/normal"..tostring(sets.version)..".lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/performace.lua"))()
 end
