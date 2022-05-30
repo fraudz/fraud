f850bfa929e2e732a7ef731fccea2f09a26e180d
fraudz <105895090+fraudz@users.noreply.github.com>
2022-05-30 12:33:20 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,19 @@
 --//simple loader
+
+if not isfile('agreetos.fraud') then 
+    writefile('agreetos.fraud','false')
+end
 local supportedgames = {
     2788229376, --da hood
 }
 if table.find(supportedgames,game.PlaceId) then 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/games/'..game.PlaceId..'/load.lua'))()
-else
+    if readfile('agreetos.fraud') == 'true' then 
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/games/'..game.PlaceId..'/load.lua'))()
+    else
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/tos.lua'))()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/games/'..game.PlaceId..'/load.lua'))()
+    end
+ else
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "fraud",
         Text = "Game Unsupported\nWe're sorry :(",
