b98f6e6c9b46d281886920052e1ce0e534bbed39
fraudz <105895090+fraudz@users.noreply.github.com>
2022-05-29 18:32:45 -0700

    Update load.lua

--- games/2788229376/load.lua
+++ games/2788229376/load.lua
@@ -1,1 +1,5 @@
-
+if getgenv().sets.performace == false then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/normal"..getgenv().sets.version..".lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/performace.lua"))()
+end
