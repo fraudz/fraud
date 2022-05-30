a7bfd1e80757485dbc58a80821c1dfbfa6f9c74b
fraudz <105895090+fraudz@users.noreply.github.com>
2022-05-29 19:30:29 -0700

    Update load.lua

--- games/2788229376/load.lua
+++ games/2788229376/load.lua
@@ -7,10 +7,12 @@ if sets == nil then
     }
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "fraud";
-        Text = "your settings were not found please re-execute";
+        Text = "your settings were not found new loadstring copied";
         Duration = 5;
         Button1 = "Ok";
     })
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/fraudz/fraud/main/games/2788229376/normal0.lua"))()
+    setclipboard("getgenv().sets = {\nperformance = false, --only esp and silent aim\nversion = 3, --set to 0 to use newest version\n}\nloadstring(game:HttpGet('https://raw.githubusercontent.com/fraudz/fraud/main/loader.lua'))()")
 end
 
 if sets.performance == false then 
